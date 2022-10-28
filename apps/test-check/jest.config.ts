22a11d98ff3ee108d09aaa534863b64b08658b92
Jaimin Shah <jaimin.shah@techextensor.com>
2022-10-28 15:02:59 +0530

    initial commit

---
+++ apps/test-check/jest.config.ts
@@ -0,0 +1,21 @@
+module.exports = {
+  displayName: 'test-check',
+  preset: '../../jest.preset.ts',
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+      stringifyContentPathRegex: '\\.(html|svg)$',
+    },
+  },
+  coverageDirectory: '../../coverage/apps/test-check',
+  transform: {
+    '^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular',
+  },
+  transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
+  snapshotSerializers: [
+    'jest-preset-angular/build/serializers/no-ng-attributes',
+    'jest-preset-angular/build/serializers/ng-snapshot',
+    'jest-preset-angular/build/serializers/html-comment',
+  ],
+};
